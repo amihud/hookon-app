8a34a4d00cd7c9df3b60153c4c9248658d53247b
amihud <amihudcohen@gmail.com>
2013-11-09 16:05:32 +0200

    first version

---
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -0,0 +1,27 @@
+function ApplicationWindow(title) {
+	var self = Ti.UI.createWindow({
+		title:title,
+		backgroundColor:'white'
+	});
+	
+	var button = Ti.UI.createButton({
+		height:88,
+		width:200,
+		title:L('openWindow'),
+		top:150
+	});
+	self.add(button);
+	
+	button.addEventListener('click', function() {
+		//containingTab attribute must be set by parent tab group on
+		//the window for this work
+		self.containingTab.open(Ti.UI.createWindow({
+			title: L('newWindow'),
+			backgroundColor: 'green'
+		}));
+	});
+	
+	return self;
+};
+
+module.exports = ApplicationWindow;
